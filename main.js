f635d0b7eb6c7a017c099e885058e576e64fcbb3
Om Kharche <orkharche@gmail.com>
2024-04-10 22:12:05 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.about').click(function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    if(target.length){
+      var scrollTo = target.offset().top;
+      $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+    }
+  });
